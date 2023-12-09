768c4af46b6a522d216ec358e155c4b43df8329f
lee-kibugi <Linda.Kibugi@student.Moringa>
2023-12-09 23:04:14 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Bears (
+  id INTEGER PRIMARY KEY,
+  name STRING,
+  age INTEGER,
+  sex STRING,
+  color STRING,
+  temperament STRING,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO Bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+  ('Tabitha', 4, 'F', 'White', 'Playful', 1),
+  ('Sergeant Brown', 7, 'M', 'Brown', 'Strict', 1),
+  ('Melissa', 2, 'F', 'Golden', 'Calm', 1),
+  ('Grinch', 6, 'M', 'Green', 'Mischievous', 1),
+  ('Wendy', 4, 'F', 'Gray', 'Curious', 1),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
